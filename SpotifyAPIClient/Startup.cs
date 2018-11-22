41c20bced823b908ae9ace62a9d40b5c4d65a0df
Potula, Chinna Rao <chinna-rao.potula@capgemini.com>
2018-11-22 09:10:26 +0100

    Add project files.

---
+++ SpotifyAPIClient/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SpotifyAPIClient.Startup))]
+namespace SpotifyAPIClient
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
